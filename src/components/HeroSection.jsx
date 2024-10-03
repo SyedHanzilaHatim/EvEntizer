bf46cd8093d3171d78ff991ad9b80c2d549ea063
SyedHanzilaHatim <syedhanzilahatim55@gmail.com>
2024-10-03 22:26:46 +0500

    first commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-scroll";
+
+const HeroSection = () => {
+  return (
+    <section className="hero">
+      <img src="/restaurant.jpg" alt="restaurant" />
+      <div className="item">
+        <h3>EvEntizer's</h3>
+        <div>
+          <h1>BEST EVENT PLANNER IN TOWN</h1>
+          <p>
+          Turning your dream events into unforgettable realities!
+          </p>
+          <Link to="contact" spy={true} smooth={true} duration={500}>
+            BOOK NOW
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
